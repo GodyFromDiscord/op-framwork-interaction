9a071c6e4a1f6e34cd51fffd51cf85ad5d98d9fc
Jack <jackthomas20132004@gmail.com>
2022-02-13 04:19:11 +0000

    bot/status
    updates status

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,6 +1,20 @@
 const chalk = require('chalk');
 
 module.exports = client => {
+
+    setInterval(() => {
+        let status = [
+            `!help | Watching the OP-FW API`,
+            `Stop Metagaming | !help`,
+            `Gody#4788 owns me`
+          ];
+          const index = Math.floor(Math.random() * (status.length - 1) + 1);
+
+          client.editStatus(null, { name: `${status[index]}`, type: 3 })
+    }, 60000);
+
+
+
     client.logger.info(chalk.yellow.bold(`Version: ${process.env.version} | Version Control: ${process.env.version_control}`))
     client.logger.info(chalk.red.bold(`OP-FW Interactions Launching`)) // I like to make my logs look nice.
     client.logger.info(chalk.green.bold(`OP-FW Interaction Bot is operational`))
